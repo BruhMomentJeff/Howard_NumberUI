cc40fbd21e0519a391883b3d2715491337797582
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-27 22:51:59 -0500

    (Import & Refactor Code)

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int intmax;
+    [SerializeField] int intmin;
+    int intGuess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        intmax = intmax + 1;
+        intGuess = (intmax + intmin) / 2;
+    }
+    
+    public void OnPressHigher()
+    {
+        intmin = intGuess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        intmax = intGuess;
+        NextGuess();
+    }
+    
+    void NextGuess()
+    {
+        intGuess = (intmax + intmin) / 2;
+    }
+
+}
